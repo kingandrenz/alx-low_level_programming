573156ac9a795bc154f7b76c52f6c186b40646c3
kingandrenz <kanuchibueze@gmail.com>
2022-09-11 09:05:21 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,14 +3,14 @@
 /**
  * main - prints all single digit number in base 10
  *
- * Return: Always 0 (success).
+ * Return: Always 0 .
  **/
 
 int main(void)
 {
 	int num;
 
-	for (num = 0; num < 0; num++)
+	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
 	putchar('\n');
