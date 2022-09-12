44d1dead8d20fb61b2297e6166b4e9df786ee339
kingandrenz <kanuchibueze@gmail.com>
2022-09-11 20:20:24 -0700

    prints differen possible combination of double digts

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	int num, num1;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num < 9; num++)
 	{
 		{
-			for (num1 = 0; num1 < 10; num1++)
+			for (num1 = num + 1; num1 < 10; num1++)
 			{
 				putchar((num % 10) + '0');
 				putchar((num1 % 10) + '0');
-				if (num == 9 && num1 == 9)
+				if (num == 8 && num1 == 9)
 
 					continue;
 
