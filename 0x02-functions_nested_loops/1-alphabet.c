fc54fcde522c48d6aa6cf4b4705a90c8ed1f7b3e
kingandrenz <kanuchibueze@gmail.com>
2022-09-13 21:50:45 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints alphabeth
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	for (char i = 'a'; i < 'z'; i++)
+		_putchar(i);
+
+	_putchar('\n');
+
+	return (0);
+}
