15e40d97ee2ee802b7a6d33287bd45d2b6ac2fa9
kingandrenz <kanuchibueze@gmail.com>
2022-09-15 08:25:52 -0700

    deleted main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,21 +5,12 @@
 * Return: 0
 */
 
-void positive_or_negative(int i)
+int main(void)
 {
-	int i;
+        int i;
 
-	for (i = 0; i <= 98; i++)
-	{
-		if (i % 2 != 0)
-		{
-			printf("%d is negative\n", i);
-			continue;
-		}
-		else
-		{
-			printf("%d is positive\n", i);
-			break;
-		}
-	}
-}	return (0);
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
+}
