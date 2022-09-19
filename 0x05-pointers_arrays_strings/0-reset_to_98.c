4d36c53cfba2352730d544923ce4b49c156fbfa0
kingandrenz <kanuchibueze@gmail.com>
2022-09-19 08:40:05 -0700

    testing code

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - set the integer to 98
+ * @i: a pointer the integer we want to set to 98
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
