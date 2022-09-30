a7360464f6e1c3177d1d952e4219fa85cc59274c
kingandrenz <kanuchibueze@gmail.com>
2022-09-29 19:58:52 -0700

    prime mumber

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <math.h>
+#include <stdbool.h>
 /**
  * is_prime_number - prints prime number
  * @n: number to check
@@ -9,10 +10,22 @@
 
 int is_prime_number(int n)
 {
+	is_prime_number = true;
 	for (i = sqrt(n); i > 1; i--)
+	{
 		if (n % i == 0)
 		{
-			return (0);
+			is_prime_number = false;
 		}
-	return (1);
+
+
+		if (is_prime_number)
+		{
+			_putchar("1\n");
+		}
+		else
+		{
+			_putchar("0\n");
+		}
+	}
 }
