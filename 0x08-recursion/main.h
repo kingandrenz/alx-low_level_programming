f7d8fc16e25a8221c7f4a455bb89a3e035b7fd1e
kingandrenz <kanuchibueze@gmail.com>
2022-09-29 20:31:52 -0700

    palidrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -20,4 +20,12 @@ int hai(int n, int d);
 
 int _sqrt(int x, int i);
 
+int is_palindrome(char *s);
 
+int get_length(char *s);
+
+int my_pal(char *s, int l);
+
+int wildcmp(char *s1, char *s2);
+
+int findsrc(char *s, char c, int i, int p);
