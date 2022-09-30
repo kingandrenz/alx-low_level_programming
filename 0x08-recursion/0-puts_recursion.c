0cd93e226cb8c529a149e915001511ff7e8c7409
kingandrenz <kanuchibueze@gmail.com>
2022-09-29 18:03:30 -0700

    intial commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,10 +9,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s <= 'z')
+	if (*s == '\0')
+		_putchar('\n');
+	else
 	{
 		_putchar(*s);
-		_putchar('\n');
-		_puts_recursion(s++);
+		_puts_recursion(++s);
 	}
 }
