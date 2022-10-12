532bea65f326a7cff799471f8da5bf2bacb5740e
kingandrenz <kanuchibueze@gmail.com>
2022-10-11 18:55:21 -0700

    initial commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,14 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+
+/**
+ * struct dog - this the the main variable
+ *
+ *@name: dogs name
+ *@owner: dogs owner
+ *@age: dogs age
+ */
+
 struct dog
 {
 	char *name;
