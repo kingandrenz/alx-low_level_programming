618187d1a34fa2e3eb4425224e0913eff93b4108
kingandrenz <kanuchibueze@gmail.com>
2022-10-14 21:00:23 -0700

    initial commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,13 +10,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list arglist;
-	int i;
+	unsigned int i;
 
-	if (n == 0)
-		return (0);
 	int sum = 0;
 
 	va_start(arglist, n);
+	if (n == 0)
+		return (0);
 	for (i = 0; i < n; i++)
 		sum += va_arg(arglist, int);
 
