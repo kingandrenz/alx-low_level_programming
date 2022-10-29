2cd9b7dfde133e39652bda19c1f39fea9d12c08e
kanu <kanuchibueze@gmail.com>
2022-10-28 19:56:45 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,6 +6,7 @@
  * @index: index starting from 0, of the bit we want to get
  * Return: Value of bit at index, or -1 if error
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int hold;
