1c432dcfdcf7cc4c3bc8e548d4630250fde7abc0
Kanu Anthony chibueze <70319065+kingandrenz@users.noreply.github.com>
2024-01-30 21:09:14 +0100

    Update 7-print_last_digit.c
    another code fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,13 @@
 
 int print_last_digit(int n)
 {
+	int a;
+	
 	if (n < 0)
 		n = n * -1;
 	
-	n = n % 10;
-	_putchar(n + '0');
-	return (n);
+        a = n % 10;
+        _putchar(a + '0');
+
+        return (a);
 }
