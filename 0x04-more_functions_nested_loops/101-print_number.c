09b6d5bc2217d72f948ece938d91673251eb7258
Andrenz <kanuchibueze@gmail.com>
2024-02-10 04:32:20 -0800

    minor fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,18 +8,16 @@
  */
 void print_number(int n)
 {
-	int temp;
 
 	if (n == INT_MIN)
 	{
 		_putchar('-');
-		temp = -(n / 10);
+		n = -(n / 10);
 
 		_putchar('0' + (temp % 10));
 		n = temp;
 	}
-
-	if (n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
