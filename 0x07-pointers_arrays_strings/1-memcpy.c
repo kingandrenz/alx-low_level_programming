84da7474e19d3726a234cb860bdfa96cc66335bc
Kanu Anthony chibueze <70319065+kingandrenz@users.noreply.github.com>
2024-03-06 06:12:40 +0100

    Update 1-memcpy.c
    correction made to code

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,15 +3,14 @@
 
 /**
  * _strchr - locate character in a string
- * @s: char array string
- * @c: char to look for
- * Return: NULL if char not found, or pointer to first occurrence of char `c`
+ * @dest: memory location to be copied to
+ * @src: memory location to be copied from
+ * Return: pointer to dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *dest1 = dest;
-	char *src1 = src;
+	unsigned int = i;
 
 	for (int i = 0; i < n; i++)
 		dest[i] = src[i];
